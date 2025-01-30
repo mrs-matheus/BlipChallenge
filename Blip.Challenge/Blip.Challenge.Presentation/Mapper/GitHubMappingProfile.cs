1806d28f1bbd2d27710c5d4ff3b2890be742fe8d
Matheus Reis <mrs.matheusreis@gmail.com>
2025-01-30 18:28:35 -0300

    Ajuste na api e config de publicação

--- Blip.Challenge/Blip.Challenge.Presentation/Mapper/GitHubMappingProfile.cs
+++ Blip.Challenge/Blip.Challenge.Presentation/Mapper/GitHubMappingProfile.cs
@@ -13,7 +13,7 @@ namespace Blip.Challenge.Presentation.Mapper
                 .ForMember(x => x.Id, opt => opt.MapFrom(y => y.Id))
                 .ForMember(x => x.Title, opt => opt.MapFrom(y => y.Name))
                 .ForMember(x => x.Description, opt => opt.MapFrom(y => y.Description))
-                .ForMember(x => x.Picture_Url, opt => opt.MapFrom(y => y.Owner.Avatar_Url))
+                .ForMember(x => x.Picture_url, opt => opt.MapFrom(y => y.Owner.Avatar_Url))
                 .ForMember(x => x.Url, opt => opt.MapFrom(y => y.Url))
                 .ForMember(x => x.Created, opt => opt.MapFrom(y => y.Created_At));
         }
