df705bfd2740577acccebd41c2f9da07d2253c1b
Matheus Reis <mrs.matheusreis@gmail.com>
2025-01-30 12:59:52 -0300

    Remoção de config Railway

--- Blip.Challenge/Blip.Challenge.Api/Program.cs
+++ Blip.Challenge/Blip.Challenge.Api/Program.cs
@@ -27,11 +27,6 @@ namespace Blip.Challenge.Api
                 app.UseSwaggerUI();
             }
 
-            //railway
-            app.UseHttpsRedirection();
-            if (builder.Environment.IsProduction() && builder.Configuration.GetValue<int?>("PORT") is not null)
-                builder.WebHost.UseUrls($"http://*:{builder.Configuration.GetValue<int>("PORT")}");
-
             app.UseAuthorization();
 
             app.UseHttpsRedirection();
