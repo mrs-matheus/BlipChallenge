c14ef9db30c8690e458d5039dd477f30c6686b6a
Matheus Reis <mrs.matheusreis@gmail.com>
2025-01-29 23:50:48 -0300

    Mais um Teste

--- Blip.Challenge/Blip.Challenge.Api/Program.cs
+++ Blip.Challenge/Blip.Challenge.Api/Program.cs
@@ -2,7 +2,7 @@ using Blip.Challenge.Api.Configs;
 
 namespace Blip.Challenge.Api
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
